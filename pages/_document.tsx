b5048995310d3ac10b88836c813cd6e6a5ee0834
jzakotnik <jure@zakotnik.de>
2024-01-14 19:10:32 +0100

    switched to http refresh tag due to no rendering on focus in kiosk mode

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {" "}
+        <meta charSet="utf-8" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta http-equiv="refresh" content="60" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
