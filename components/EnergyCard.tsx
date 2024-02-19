c640984468b554ab568af9d39c5f421988365388
jzakotnik <jure@zakotnik.de>
2024-02-19 16:30:11 +0100

    updated PV card

--- components/EnergyCard.tsx
+++ components/EnergyCard.tsx
@@ -3,11 +3,14 @@ import {
   Card,
   CardContent,
   CardHeader,
+  Grid,
   Typography,
   useTheme,
 } from "@mui/material";
 import DirectionsCarFilledIcon from "@mui/icons-material/DirectionsCarFilled";
 import SolarPowerIcon from "@mui/icons-material/SolarPower";
+import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
+import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
 
 type EnergyCardProps = {
   energy: any;
@@ -33,12 +36,27 @@ export default function EnergyCard({ energy }: EnergyCardProps) {
     >
       <CardHeader
         title={
-          <Typography variant="h5" sx={{ fontWeight: "bold" }}>
-            {energy.energy.data.ppv.toString() +
-              " - " +
-              energy.energy.data.pgrid.toString() +
-              " W"}
-          </Typography>
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-around"
+            alignItems="stretch"
+          >
+            <Grid item sx={{ mx: 1 }}>
+              {" "}
+              {energy.energy.data.ppv - energy.energy.data.pgrid < 0 ? (
+                <ArrowCircleDownIcon />
+              ) : (
+                <ArrowCircleUpIcon />
+              )}
+            </Grid>
+            <Grid item>
+              {" "}
+              <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                {"PV"}
+              </Typography>
+            </Grid>
+          </Grid>
         }
         avatar={
           <Avatar sx={{ bgcolor: "#FAEAFF" }} aria-label="icon">
@@ -48,7 +66,13 @@ export default function EnergyCard({ energy }: EnergyCardProps) {
       />
       <CardContent>
         <Typography variant="h5" sx={{ fontWeight: "bold" }}>
-          PV
+          {"PV: " + energy.energy.data.ppv.toString() + " W"}
+        </Typography>
+        <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+          {"Grid: " + energy.energy.data.pgrid.toString() + " W"}{" "}
+        </Typography>
+        <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+          {"SoC: " + energy.energy.data.soc.toString()}
         </Typography>
       </CardContent>
     </Card>
